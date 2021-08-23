0aec0976b4d181dc896234bbbaca7591bee4220e
Caio Fugii <caio.fugii.ext@infracommerce.com.br>
2021-08-23 14:36:01 -0300

    adjust JWT middleware / indexes Order

--- src/utils/middlewares/configMiddleware.ts
+++ src/utils/middlewares/configMiddleware.ts
@@ -2,7 +2,7 @@ import { Response, Next, helpers } from "ihub-framework-ts";
 import { IRequest } from '../../common/interfaces/request';
 import { ConfigService } from "../../components/configs/services/configService";
 
-import JWT from '../JwtUtils';
+import { JWTUtils } from '../JwtUtils';
 
 const { HttpHelper } = helpers;
 
@@ -11,17 +11,27 @@ export default async (req: IRequest, res: Response, next: Next) => {
         const storeId = req.headers["x-cxaas-accountid"];
         const token = req.headers["authorization"];
 
-        const { stores, email } = JWT.decode(token)
+        if(!storeId || !token){
+            return HttpHelper.clientError(res, "Missing Headers X-CXAAS-AccountId / Authorization");
+        }
+
+        const jwtPayload = JWTUtils.decode(token);
+
+        if(jwtPayload.hasError){
+            return HttpHelper.unauthorized(res, jwtPayload.error)
+        }
+
+        const { stores, email } = jwtPayload.data
 
         if(!stores.includes(storeId)){
-            return HttpHelper.unauthorized(res)
+            return HttpHelper.forbidden(res, "This user does not have access to this Account")
         }
 
         const configService = new ConfigService();
         const config = await configService.findStoreConfigById(String(storeId));
 
         if (!config || !config.active){
-            return HttpHelper.unauthorized(
+            return HttpHelper.notFound(
                 res,
                 "The configuration wasn't found for this store"
             );
