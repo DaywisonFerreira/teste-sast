8e8ce546d842db98096e2e9b718fd17a0485b1fb
pedropereiraifc <pedro.pereira.ext@infracommerce.com.br>
2021-10-09 18:06:36 -0300

    chore: re-activate intelipost endpoint

--- src/components/root/routes/route.ts
+++ src/components/root/routes/route.ts
@@ -18,7 +18,7 @@ export default [
     },
     {
         method: 'post',
-        path: '/intelipost-test',
+        path: '/intelipost',
         private: false,
         controller: postIntelipost,
     },
