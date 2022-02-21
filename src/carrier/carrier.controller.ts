349785412aeb5d2b04116010ec50a73f8e93e31c
Robson Durval <robson.ferreira.ext@infracommerce.com.br>
2022-02-21 21:48:24 +0000

    Changing file upload

--- src/carrier/carrier.controller.ts
+++ src/carrier/carrier.controller.ts
@@ -2,7 +2,7 @@ import {
   Body,
   Controller,
   Get,
-  Headers,
+  // Headers,
   Inject,
   Param,
   Patch,
@@ -16,20 +16,21 @@ import {
   ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
-import { createReadStream, existsSync, unlinkSync } from 'fs';
+import { existsSync, unlinkSync } from 'fs';
 import { FastifyFileInterceptor } from 'src/commons/interceptors/file.interceptor';
 import { diskStorage } from 'multer';
 import { Env } from 'src/commons/environment/env';
-import axios from 'axios';
-import * as FormData from 'form-data';
+// import axios from 'axios';
+// import * as FormData from 'form-data';
 import { LogProvider } from '@infralabs/infra-logger';
+import { createBlobService } from 'azure-storage';
 import { JWTGuard } from '../commons/guards/jwt.guard';
 import { GetCarrierDto } from './dto/get-carrier.dto';
 import { UpdateCarrierDto } from './dto/update-carrier.dto';
 import { CarrierService } from './carrier.service';
 import { EditFileName, FileFilter } from './mappers';
 import { UploadLogoDto } from './dto/upload-logo.dto';
-import { HeadersDto } from './dto/headers.dto';
+// import { HeadersDto } from './dto/headers.dto';
 
 @Controller('carrier')
 @ApiTags('Carrier')
@@ -66,6 +67,30 @@ export class CarrierController {
     return GetCarrierDto.factory(carrier) as GetCarrierDto;
   }
 
+  private async uploadFile(
+    fileLocally: any,
+    localFileName: string,
+  ): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const blobSvc = createBlobService(String(Env.AZURE_BS_ACCESS_KEY));
+      blobSvc.createBlockBlobFromLocalFile(
+        String(Env.AZURE_BS_CONTAINER_NAME),
+        localFileName,
+        fileLocally.path,
+        error => {
+          if (error) {
+            reject(error);
+          }
+          resolve(
+            `${String(Env.AZURE_BS_STORAGE_URL)}/${String(
+              process.env.AZURE_BS_CONTAINER_NAME,
+            )}/${localFileName}`,
+          );
+        },
+      );
+    });
+  }
+
   @Patch(':id/logo')
   @UseGuards(JWTGuard)
   @ApiConsumes('multipart/form-data')
@@ -84,45 +109,45 @@ export class CarrierController {
   )
   async uploadCarrierLogo(
     @UploadedFile() file: any,
-    @Headers() headers: HeadersDto,
+    // @Headers() headers: HeadersDto,
     @Param('id') id: string,
     @Body() _: UploadLogoDto,
   ) {
-    const fileLocally = file;
+    const localFileName = `${file.filename}_${file.originalname}`;
+    const fileLocally = await this.uploadFile(file, localFileName);
     try {
-      const data = new FormData();
-      data.append('file', createReadStream(file.path), {
-        contentType: file.mimetype,
-      });
-      data.append('canonical', 'carriers-logo');
-      data.append('context', 'freight');
-      data.append('contentType', file.mimetype);
-      data.append('contentLanguage', 'en-us');
-      data.append('cacheControl', 'no-cache');
-
-      const config: any = {
-        url: Env.CONTENT_API_URI,
-        method: 'POST',
-        headers: {
-          'X-Tenant-Id': headers['x-tenant-id'],
-          'X-Channel-Id': headers['x-channel-id'],
-          Authorization: headers.authorization,
-          ...data.getHeaders(),
-        },
-        data,
-      };
-
-      const response: any = await axios(config);
-
       await this.carrierService.updateLogo(id, {
-        logo: response.data.data.uri,
+        logo: `${fileLocally}`,
       });
+      //   const data = new FormData();
+      //   data.append('file', createReadStream(file.path), {
+      //     contentType: file.mimetype,
+      //   });
+      //   data.append('canonical', 'carriers-logo');
+      //   data.append('context', 'freight');
+      //   data.append('contentType', file.mimetype);
+      //   data.append('contentLanguage', 'en-us');
+      //   data.append('cacheControl', 'no-cache');
+
+      //   const config: any = {
+      //     url: Env.CONTENT_API_URI,
+      //     method: 'POST',
+      //     headers: {
+      //       'X-Tenant-Id': headers['x-tenant-id'],
+      //       'X-Channel-Id': headers['x-channel-id'],
+      //       Authorization: headers.authorization,
+      //       ...data.getHeaders(),
+      //     },
+      //     data,
+      //   };
+
+      //   const response: any = await axios(config);
     } catch (error) {
       this.logger.log(error);
       throw error;
     } finally {
-      if (existsSync(fileLocally.path)) {
-        unlinkSync(fileLocally.path);
+      if (existsSync(file.path)) {
+        unlinkSync(file.path);
       }
     }
   }
