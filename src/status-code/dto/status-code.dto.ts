e14c3ddd434871164913c66da7b95d5df66de45c
Robson Durval <robson.ferreira.ext@infracommerce.com.br>
2022-04-04 14:56:42 -0300

    C30F-1053 swagger: changing type property on endpoint to get statusCode

--- src/status-code/dto/status-code.dto.ts
+++ src/status-code/dto/status-code.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { v4 as uuidV4 } from 'uuid';
 
 export class GetStatusCodeDto {
   @ApiProperty({ example: '616de48e7b1e23aa6ec7204d' })
   id: string;
 
-  @ApiProperty({ example: '616de48e7b1e23aa6ec7204d' })
+  @ApiProperty({ example: uuidV4() || null })
   parentId: string;
 
   @ApiProperty({ example: 'Status Code Name' })
