11243c99ac75bb522740ccaf4d34d24dbd686c24
Robson Durval <robson.ferreira.ext@infracommerce.com.br>
2022-04-06 17:42:36 -0300

    C30F-1053 refector:  updating statusCode entity

--- src/status-code/schemas/status-code.schema.ts
+++ src/status-code/schemas/status-code.schema.ts
@@ -2,15 +2,15 @@ import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { v4 as uuidV4 } from 'uuid';
 
-@Schema({ collection: 'status-codes', timestamps: true })
+@Schema({ collection: 'statusCodes', timestamps: true })
 export class StatusCodeEntity extends Document {
   @Prop({ type: String, required: true, default: uuidV4() })
   id: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: false, default: null })
   parentId: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, unique: true })
   name: string;
 }
 
