29c233f30280de7636cc743bcd6d44a6e50cad04
Robson Durval <robson.ferreira.ext@infracommerce.com.br>
2022-04-06 17:57:20 -0300

    C30F-1053 chore: adding seed to statusCode

---
+++ src/status-code/seeder/data/index.ts
@@ -0,0 +1,30 @@
+export const macroStatus = [
+  {
+    name: 'in-transit',
+    parentId: null,
+  },
+  {
+    name: 'delivery-failed',
+    parentId: null,
+  },
+  {
+    name: 'delivered',
+    parentId: null,
+  },
+  {
+    name: 'canceled',
+    parentId: null,
+  },
+  {
+    name: 'order-created',
+    parentId: null,
+  },
+  {
+    name: 'out-for-delivery',
+    parentId: null,
+  },
+  {
+    name: 'order-dispatched',
+    parentId: null,
+  },
+];
