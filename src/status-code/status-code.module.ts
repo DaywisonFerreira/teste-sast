04a549ecbd25800388ba6164f6d70241eaff362f
pedropereiraifc <pedro.pereira.ext@infracommerce.com.br>
2022-04-07 17:56:18 -0300

    chore: adjustments

--- src/status-code/status-code.module.ts
+++ src/status-code/status-code.module.ts
@@ -1,8 +1,5 @@
 import { MongooseModule } from '@nestjs/mongoose';
-import { Module, Scope } from '@nestjs/common';
-import { Env } from 'src/commons/environment/env';
-import { NestjsLogger } from 'src/commons/providers/log/nestjs-logger';
-import { InfraLogger as Logger } from '@infralabs/infra-logger';
+import { Module } from '@nestjs/common';
 import { StatusCodeService } from './status-code.service';
 import { StatusCodeController } from './status-code.controller';
 import {
@@ -20,14 +17,7 @@ import {
     ]),
   ],
   controllers: [StatusCodeController],
-  providers: [
-    {
-      provide: 'LogProvider',
-      useClass: Env.NODE_ENV === 'local' ? NestjsLogger : Logger,
-      scope: Scope.TRANSIENT,
-    },
-    StatusCodeService,
-  ],
+  providers: [StatusCodeService],
   exports: [
     MongooseModule.forFeature([
       {
