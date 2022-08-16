55180849eb121e741f9ac5c40ef41a37265156f7
pedropereiraifc <pedro.pereira.ext@infracommerce.com.br>
2022-08-16 19:28:08 -0300

    chore: updates

--- src/intelipost/factories/index.ts
+++ src/intelipost/factories/index.ts
@@ -38,6 +38,9 @@ export const MessageOrderNotified = content => {
 
   orderMapper.invoice = {
     value: payload.invoice?.value || 0,
+    number: payload.invoice?.number || '',
+    trackingUrl: payload.invoice?.trackingUrl || '',
+    customerDocument: payload.invoice?.customerDocument || '',
   };
 
   orderMapper.carrier = {
@@ -53,6 +56,7 @@ export const MessageOrderNotified = content => {
     orderMapper.customer = {
       firstName: payload.customer?.firstName || '',
       lastName: payload.customer?.lastName || '',
+      fullName: payload.customer?.fullName || '',
       document: payload.customer?.document || '',
       documentType: payload.customer?.documentType || '',
     };
@@ -60,7 +64,10 @@ export const MessageOrderNotified = content => {
 
   orderMapper.shippingEstimateDate =
     payload.estimateDeliveryDateDeliveryCompany;
-  orderMapper.trackingUrl = payload.invoice?.trackingUrl || '';
+
+  orderMapper.deliveryCompany = payload.logisticInfo?.length
+    ? payload.logisticInfo[0].deliveryCompany || ''
+    : '';
 
   return {
     headers: {
