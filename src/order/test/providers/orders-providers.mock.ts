b863ad1acd17f4286e41d73ce3f87f77d5f1c214
pedropereiraifc <pedro.pereira.ext@infracommerce.com.br>
2022-09-16 01:30:17 -0300

    chore: improvements test

---
+++ src/order/test/providers/orders-providers.mock.ts
@@ -0,0 +1,19 @@
+import { getModelToken } from '@nestjs/mongoose';
+
+import { AccountEntity } from '../../../account/schemas/account.schema';
+import { OrderService } from '../../order.service';
+import { OrderEntity } from '../../schemas/order.schema';
+import { OrdersModelMock } from './orders-model.mock';
+
+export const OrdersProvidersMock = [
+  OrderService,
+  {
+    provide: getModelToken(OrderEntity.name),
+    useValue: OrdersModelMock,
+  },
+  {
+    provide: getModelToken(AccountEntity.name),
+    useValue: {},
+  },
+  { provide: 'KafkaService', useValue: {} },
+];
