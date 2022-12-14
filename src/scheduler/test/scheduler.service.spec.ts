fb23138f6f785fe2908ef54074d7b491d5d05a98
Caio Fugii <caio.fugii.ext@infracommerce.com.br>
2022-12-14 14:21:36 -0300

    unit tests

---
+++ src/scheduler/test/scheduler.service.spec.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Test, TestingModule } from '@nestjs/testing';
+import * as fs from 'fs/promises';
+import { SchedulerService } from '../scheduler.service';
+
+describe('Scheduler Service Tests', () => {
+  let service: SchedulerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SchedulerService],
+    }).compile();
+
+    service = await module.resolve(SchedulerService);
+    jest.clearAllMocks();
+  });
+
+  it('Should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('clearTmpFolder', () => {
+    it('Should remove older files from TMP folder', async () => {
+      const olderDate = new Date();
+      olderDate.setDate(new Date().getDate() - 12);
+
+      const spyFsReaddir = jest
+        .spyOn(fs, 'readdir')
+        // @ts-ignore
+        .mockResolvedValueOnce(['file_one', 'file_two']);
+
+      const spyFsStat = jest
+        .spyOn(fs, 'stat')
+        // @ts-ignore
+        .mockResolvedValue({ birthtime: olderDate });
+
+      const spyFsUnlink = jest
+        .spyOn(fs, 'unlink')
+        // @ts-ignore
+        .mockResolvedValueOnce(undefined);
+
+      await service.clearTmpFolder();
+
+      expect(spyFsReaddir).toBeCalledTimes(1);
+      expect(spyFsStat).toBeCalledTimes(2);
+      expect(spyFsUnlink).toBeCalledTimes(2);
+    });
+
+    it('Should ignore newest files from TMP folder', async () => {
+      const newestDate = new Date();
+      newestDate.setDate(new Date().getDate() + 12);
+
+      const spyFsReaddir = jest
+        .spyOn(fs, 'readdir')
+        // @ts-ignore
+        .mockResolvedValueOnce(['file_one', 'file_two']);
+
+      const spyFsStat = jest
+        .spyOn(fs, 'stat')
+        // @ts-ignore
+        .mockResolvedValue({ birthtime: newestDate });
+
+      const spyFsUnlink = jest
+        .spyOn(fs, 'unlink')
+        // @ts-ignore
+        .mockResolvedValueOnce(undefined);
+
+      await service.clearTmpFolder();
+
+      expect(spyFsReaddir).toBeCalledTimes(1);
+      expect(spyFsStat).toBeCalledTimes(2);
+      expect(spyFsUnlink).not.toBeCalled();
+    });
+
+    it('Should ignore if not exist files on folder TMP', async () => {
+      const spyFsReaddir = jest
+        .spyOn(fs, 'readdir')
+        // @ts-ignore
+        .mockResolvedValueOnce([]);
+
+      const spyFsStat = jest.spyOn(fs, 'stat');
+
+      const spyFsUnlink = jest.spyOn(fs, 'unlink');
+
+      await service.clearTmpFolder();
+
+      expect(spyFsReaddir).toBeCalledTimes(1);
+      expect(spyFsStat).not.toBeCalled();
+      expect(spyFsUnlink).not.toBeCalled();
+    });
+  });
+});
