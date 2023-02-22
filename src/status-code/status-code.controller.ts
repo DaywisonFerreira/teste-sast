4f3f3b803afea3521959232e8066966774853004
Daywison Leal <daywison.leal@infracommerce.com.br>
2023-02-22 15:29:46 -0300

    feat: create endpoint to list macro and micro status

--- src/status-code/status-code.controller.ts
+++ src/status-code/status-code.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Response } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { LogProvider } from 'src/commons/providers/log/log-provider.interface';
-import { GetStatusCodeDto } from './dto/status-code.dto';
+import {
+  GetStatusCodeMacroDto,
+  GetStatusCodeMicroDto,
+} from './dto/status-code.dto';
 import { StatusCodeService } from './status-code.service';
 
 @Controller('status-code')
@@ -15,18 +18,41 @@ export class StatusCodeController {
     this.logger.instanceLogger(StatusCodeController.name);
   }
 
-  @Get('get-list')
-  @ApiOkResponse({ type: [GetStatusCodeDto] })
-  async getStatusCode(): Promise<Array<GetStatusCodeDto>> {
+  @Get('macro')
+  @ApiOkResponse({ type: [GetStatusCodeMacroDto] })
+  async getStatusCodeMacro(
+    @Response() res,
+  ): Promise<Array<GetStatusCodeMacroDto>> {
     try {
-      this.logger.log(
-        {
-          key: 'ifc.freight.api.order.status-code-controller.getStatusCode',
-          message: `Get all status codes`,
-        },
-        {},
+      this.logger.log({
+        key: 'ifc.freight.api.order.status-code-controller.getStatusCodeMacro',
+        message: `Get all status codes macro`,
+      });
+      const results = await this.statusCodeService.getStatusCodeMacroList();
+      return res.send(
+        results.map(result => GetStatusCodeMacroDto.factory(result as any)),
       );
-      return this.statusCodeService.getList();
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
+    }
+  }
+
+  @Get('macro/:code/micro')
+  @ApiOkResponse({ type: [GetStatusCodeMicroDto] })
+  async getStatusCodeMicro(
+    @Param('code') code: string,
+    @Response() res,
+  ): Promise<Array<GetStatusCodeMicroDto>> {
+    try {
+      this.logger.log({
+        key: 'ifc.freight.api.order.status-code-controller.getStatusCodeMicro',
+        message: `Get all status codes micro of macro: ${code}`,
+      });
+      const results = await this.statusCodeService.getStatusCodeMicroList(code);
+      return res.send(
+        results.map(result => GetStatusCodeMicroDto.factory(result as any)),
+      );
     } catch (error) {
       this.logger.error(error);
       throw error;
